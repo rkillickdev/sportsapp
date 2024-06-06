66314b99e61d18aaf436f40aa9fe758033163f9e
Rob Killick <robkillick@Robs-MacBook-Pro-2.local>
2024-06-06 14:22:23 +0100

    setup code for user authentication

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import UserNav from './UserNav';
+import { getUserId } from '@/app/lib/actions';
+
+const Navbar = async () => {
+    const userId = await getUserId();
+
+    console.log('userId:', userId);
+
+    return (
+        <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+            <div className="max-w-[1500px] mx-auto px-6">
+                <div className="flex justify-between items-center">
+                    <Link href="/">
+                        <Image
+                            src="/logo.png"
+                            alt="DjangoBnb logo"
+                            width={180}
+                            height={38}
+                        />
+                    </Link>
+
+                    <div className="flex space-x-6">
+                        Search Filters
+                    </div>
+
+                    <div className="flex items-center space-x-6">
+                        <UserNav
+                            userId={userId}
+                        />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
