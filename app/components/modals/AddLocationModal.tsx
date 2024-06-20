5971c19d970ff07ca041bd7527e0f64fef44a81d
Rob Killick <robkillick@Robs-MacBook-Pro-2.local>
2024-06-20 13:47:59 +0100

    implement functionality to post location data to the backend api

--- app/components/modals/AddLocationModal.tsx
+++ app/components/modals/AddLocationModal.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 
 import { useState } from 'react';
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 import Modal from './Modal';
 
 import LoginModal from './LoginModal';
@@ -10,11 +11,69 @@ import useAddLocationModal from '@/app/hooks/useAddLocationModal';
 import CustomButton from '../forms/CustomButton';
 import SelectCountry, { SelectCountryValue } from '../forms/SelectCountry';
 
+import apiService from '@/app/services/apiService';
+import { useRouter } from 'next/navigation';
+
 const AddLocationModal = () => {
   const [currentStep, setCurrentStep] = useState(1);
-  const [dataCountry, setDataCountry] = useState<SelectCountryValue>();
+  const [errors, setErrors] = useState<string[]>([]);
+  const [country, setCountry] = useState('');
+  const [region, setRegion] = useState('');
+  const [name, setName] = useState('');
+  const [summary, setSummary] = useState('');
 
   const addLocationModal = useAddLocationModal();
+  const router = useRouter();
+
+  const submitForm = async () => {
+    console.log('submit form');
+
+    if (
+      country &&
+      region &&
+      name &&
+      summary
+    ) {
+        const formData = new FormData();
+        formData.append('country', country);
+        formData.append('region', region);
+        formData.append('name', name);
+        formData.append('summary', summary);
+
+        try {
+          const data = await apiService.post('/api/location/locations/', formData);
+          console.log('SUCCESS :-D');
+          console.log(data)
+
+          router.push('/');
+          setCurrentStep(1);
+
+          addLocationModal.close();
+        } catch (err) {
+          console.log(err);
+        }
+
+        // const response = await apiService.post('/api/location/locations/', formData);
+        // console.log(response);
+
+        // if (response.success) {
+        //   console.log('SUCCESS :-D');
+
+        //   router.push('/');
+
+        //   addLocationModal.close();
+        // } else {
+        //   console.log('Error');
+
+        //   const tmpErrors: string[] = Object.values(response).map((error: any) => {
+        //     return error;
+        //   })
+
+        //   setErrors(tmpErrors)
+
+        // }
+      }
+  }
 
   const content = (
     <>
@@ -22,9 +81,15 @@ const AddLocationModal = () => {
         <>
           <h2 className='mb-6 text-2xl'>Location</h2>
 
-          <SelectCountry
-            value={dataCountry}
-            onChange={(value) => setDataCountry(value as SelectCountryValue)}
+          <CountryDropdown
+            value={country}
+            onChange={(val) => setCountry(val)}
+            whitelist={['GB']}
+          />
+          <RegionDropdown
+            country={country}
+            value={region}
+            onChange={(val) => setRegion(val)}
           />
 
           <CustomButton
@@ -33,8 +98,31 @@ const AddLocationModal = () => {
           />
         </>
 
-      ) : (
+      ) : currentStep == 2 ? (
         <>
+          <h2 className='mb-6 text-2xl'>Describe your place</h2>
+
+          <div className='pt-3 pb-6 space-y-4'>
+            <div className='flex flex-col space-y-2'>
+                <label>Name</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className='w-full p-4 border border-gray-600 rounded-xl'
+                />
+            </div>
+
+            <div className='flex flex-col space-y-2'>
+                <label>Summary</label>
+                <textarea
+                    value={summary}
+                    onChange={(e) => setSummary(e.target.value)}
+                    className='w-full h-[200px] p-4 border border-gray-600 rounded-xl'
+                ></textarea>
+            </div>
+          </div>
+
           <CustomButton
             label='Previous'
             className='mb-2 bg-black hover:bg-gray-800'
@@ -44,6 +132,24 @@ const AddLocationModal = () => {
           <CustomButton
               label='Next'
               onClick={() => setCurrentStep(3)}
+          />
+        </>
+      ) : (
+        <>
+          {errors.map((error, index) => {
+            return (
+              <div
+                key={index}
+                className='p-5 mb-4 bg-airbnb text-white rounded-xl opacity-80'
+              >
+                {error}
+              </div>
+            )
+          })}
+
+          <CustomButton
+            label='Submit'
+            onClick={submitForm}
           />
         </>
       )}
